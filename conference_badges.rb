160040c7ea29d19d32a5b0cd0b0bd7a9fbbbd410
Jhana Gustave <jhana.gustave@gmail.com>
2019-04-17 19:14:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names_array)
+badges_array = []
+  names_array.each { |el| badges_array << badge_maker(el)}
+  badges_array
+end
+
+
+def def assign_rooms(names_array)
+  assign_rooms_array = []
+  names_array.each { |el, counter| assign_rooms_array <<
+  
+end
